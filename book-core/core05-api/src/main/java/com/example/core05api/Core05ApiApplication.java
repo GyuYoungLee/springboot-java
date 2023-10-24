240543f2d59a4460320d1ddb195032fccc673161
gyuyoung <ifos2u@naver.com>
2023-10-24 21:20:52 +0900

    231024 - book-core - core05-api - init

---
+++ book-core/core05-api/src/main/java/com/example/core05api/Core05ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.core05api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core05ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core05ApiApplication.class, args);
+	}
+
+}
