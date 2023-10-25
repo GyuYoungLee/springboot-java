befeb1355e4d49140ee6fa7fb7d066d8d58c64e4
gyuyoung <ifos2u@naver.com>
2023-10-25 17:11:41 +0900

    231025 - book-core - core08-advanced-jpa - init

---
+++ book-core/core08-advanced-jpa/src/test/java/com/example/core08advancedjpa/Core08AdvancedJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core08advancedjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core08AdvancedJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
