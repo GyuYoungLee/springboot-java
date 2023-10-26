74ac9bef9f9ed3f91dcdfb0b7f1a628950195f88
gyuyoung <ifos2u@naver.com>
2023-10-26 16:04:51 +0900

    231026 - book-core - core12-rest - init

---
+++ book-core/core12-rest/src/main/java/com/example/core12rest/Core12RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.core12rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core12RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Core12RestApplication.class, args);
+    }
+
+}
---
+++ book-core/core12-rest/src/test/java/com/example/core12rest/Core12RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core12rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core12RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
