134fcfad5b7f69ef3136a1597785b3664ed8d470
gyuyoung <ifos2u@naver.com>
2023-10-26 18:48:54 +0900

    231026 - book-core - core13-security - init

---
+++ book-core/core13-security/src/main/java/com/example/core13security/Core13SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.core13security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core13SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core13SecurityApplication.class, args);
+	}
+
+}
---
+++ book-core/core13-security/src/test/java/com/example/core13security/Core13SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core13security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core13SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
