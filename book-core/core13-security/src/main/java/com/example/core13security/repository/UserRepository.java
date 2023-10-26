e9e8ba8b264597d32526da352cf7a8f1a0fb943c
gyuyoung <ifos2u@naver.com>
2023-10-26 23:32:38 +0900

    231026 - book-core - core13-security - add security

---
+++ book-core/core13-security/src/main/java/com/example/core13security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.core13security.repository;
+
+import com.example.core13security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
